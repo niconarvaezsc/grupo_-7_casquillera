167277ef2ef1005b748434ac8af3ee9cb5757bb8
jorgedf1991 <jorgeldiaz1628@gmail.com>
2023-03-07 20:06:23 -0500

    borre json duplicado

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,11 +7,7 @@ const router = express.Router();
 router.get("/", mainController.index);
 router.get("/login", mainController.login);
 router.get("/cart", mainController.productCart);
-<<<<<<< HEAD:src/routes/main.js
 router.get("/detail/:id", mainController.detailProduc);
 router.get('/register', mainController.register)
-=======
-router.get("/detail", mainController.detailProduc);
->>>>>>> 74cc9df736ce2b8087c3feff7344d6ad8fc0e6a4:src/router/main.js
 
 module.exports = router;
